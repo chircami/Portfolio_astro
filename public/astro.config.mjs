6f1ca2a7897c845bb8855f2fb3b63cfbe489ebf1
Chircami <chircami@gmail.com>
2025-11-09 22:27:59 +0100

    Update astro.config.mjs

--- public/astro.config.mjs
+++ public/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  // Tu dominio de GitHub Pages
   site: 'https://chircami.github.io',
-  base: '/Portfolio_astro', // Solo si el repo NO es <usuario>.github.io
+  // IMPORTANTE: El nombre exacto de tu repositorio, empezando por barra
+  base: '/Portfolio_astro',
 });
