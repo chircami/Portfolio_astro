b1c4aee9b2aba62d711ae7978eb22217221b3b17
Chircami <chircami@gmail.com>
2025-11-09 22:39:27 +0100

    Update astro.config.mjs

--- public/astro.config.mjs
+++ public/astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  // NO ponemos 'site' ni 'base' para que Astro no intente generar URLs absolutas.
-  build: {
-    // Esto ayuda a mantener los archivos organizados
-    assets: '_assets'
-  }
+  site: 'https://chircami.github.io',
+  base: '/Portfolio_astro', 
 });
