9e3f7fb2bf993c019839852b8fa4cafcbfab3582
Chircami <chircami@gmail.com>
2025-11-09 23:48:58 +0100

    Update astro.config.mjs
    change

--- public/astro.config.mjs
+++ public/astro.config.mjs
@@ -1,6 +1,6 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://chircami.github.io',
   base: '/Portfolio_astro',
