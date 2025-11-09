f4f331123912c4277fdca8c7376f2848b29f9170
Mirko Zedde <mzedde@MacBook-Air-de-Mirko.local>
2025-11-09 21:51:08 +0100

    Primer commit: mi portfolio en Astro

---
+++ public/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://chircami.github.io',
+  base: '/Portfolio_astro', // Solo si el repo NO es <usuario>.github.io
+});
